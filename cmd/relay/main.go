b8b2008d99c7f187a2f8bbdf9566128c4f5ed570
Hossein Yazdani <hosseinyazdani1376@gmail.com>
2026-06-03 03:33:44 +0330

    relay: simplify version output to show kamune dependency version

--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/kamune-org/kamune/cmd/relay/run"
 )
 
-// Set via ldflags at build time.
 var (
 	version = "dev"
-	commit  = "none"
-	date    = "unknown"
 )
+
+func kamuneVersion() string {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+	for _, dep := range bi.Deps {
+		if dep.Path == "github.com/kamune-org/kamune" {
+			return dep.Version
+		}
+	}
+	return "unknown"
+}
 
 func main() {
 	if len(os.Args) > 1 && (os.Args[1] == "-version" || os.Args[1] == "--version") {
-		fmt.Printf("kamune-relay %s (commit: %s, built: %s)\n", version, commit, date)
+		fmt.Printf("kamune-relay %s (kamune: %s)\n", version, kamuneVersion())
 		return
 	}
 
